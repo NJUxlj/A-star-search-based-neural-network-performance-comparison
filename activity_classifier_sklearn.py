6a4020a89f140404fc52650d75130735348f0a3b
zhongyah <1016509070@qq.com>
2024-04-20 19:47:22 +0800

    update

--- activity_classifier_sklearn.py
+++ activity_classifier_sklearn.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from sklearn.utils import shuffle
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix,classification_report
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score, GridSearchCV
 
 
@@ -19,11 +20,21 @@ class SVMModel:
     
     
     def set_kernel(self, kernel):
+        '''
+        设置核函数
+        '''
         self.kernel = kernel
     
     def set_state(self, state):
         self.state = state
         
+    def set_kernel_params(self):
+        '''
+        设置核函数的参数，比如：
+        1. 多项式核的 h和c
+        2. rbf和的sigma， 8它控制着核函数的宽度， 影响模型的灵敏度
+        '''
+        pass
     
     def forward(self, x, y= None):
         pass
--- activity_classifier_transformer.py
+++ activity_classifier_transformer.py
@@ -32,6 +32,33 @@ labels = [1, 2, 1]  # 假设1代表经济，2代表体育
 
 
 
+class NewsDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        label = self.labels[idx]
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=64,
+            return_token_type_ids=False,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='pt',
+            truncation=True
+        )
+        return {
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'labels': torch.tensor(label)
+        }
 
 
 
